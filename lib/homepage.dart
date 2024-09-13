a3a8971916cf01c816ffc6cadf088fb8620cdebc
jaasi <jaasirahamed87@gmail.com>
2024-09-13 12:38:06 +0530

    home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,1 +1,9 @@
-//jaas
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
